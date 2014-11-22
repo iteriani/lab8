bb1784446f155ef1cdf0fc5e537575e52169c8f7
Thomas Nguyen <fortrex@gmail.com>
2014-11-21 23:59:55 -0800

    narwhal

--- app.js
+++ app.js
@@ -147,15 +147,14 @@ app.post("/message", function(req,res){
 		});
 
 	};
+});
 /*
 
 */
-	
-})
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-}
 
